c52574eb7050301cefa7866556a6f0808075407b
stbrouwers <33906225+PS223934@users.noreply.github.com>
2022-01-24 10:36:10 +0100

    Add files via upload

---
+++ laravel/routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::group(['middleware'=>'auth'], function(){
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+    Route::get('/admin/resetpassword/{id}', [AdminController::class, 'resetpassword'])->name('admin.resetpassword');
+    Route::put('/admin/updatepassword/{id}', [AdminController::class, 'updatepassword'])->name('admin.updatepassword');
+    Route::resource('role', RoleController::class);
+    Route::resource('user', UserController::class);
+    Route::resource('employee', EmployeeController::class);
+    Route::resource('customer', CustomerController::class);
+});
+
+
+require __DIR__.'/auth.php';
